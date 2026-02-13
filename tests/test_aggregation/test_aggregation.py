58328224ff29b5722b3af400842081db6ea764de
René Raab <rene.raab@lmu.de>
2026-02-13 15:24:22 +0100

    test: extend the test of the Aggregator class

--- tests/test_aggregation/test_aggregation.py
+++ tests/test_aggregation/test_aggregation.py
@@ -1,22 +1,22 @@
-import pytest
-import numpy as np
 from fedstats.aggregation.aggregator import Aggregator
+
 
 def test_aggregator_unit():
     """
     Test abstract class Aggregator
     """
     # arbitrary results from servers
-    results = [(-99, -99),  # Server 1 
-               (-99, -99),  # Server 2 
-               ]
-    Aggregator.__abstractmethods__ = set()
+    node_results = [
+        (-99, -99),  # Server 1
+        (-99, -99),  # Server 2
+    ]
+    Aggregator.__abstractmethods__ = set()  # type: ignore
 
-    agg = Aggregator(results)
-    # agg.aggregate_results() 
-    # agg.get_results() 
-    
+    agg = Aggregator(node_results)  # type: ignore
+    agg.aggregate_results()
+    agg_results = agg.get_aggregated_results()
+
     assert isinstance(agg, Aggregator)
     assert isinstance(agg.node_results, list)
-
-    
+    assert agg.node_results == node_results
+    assert agg_results is None
