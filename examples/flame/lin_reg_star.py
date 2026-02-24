19435714352fda4a306a7cbe39272b66d711dea9
René Raab <rene.raab@lmu.de>
2026-02-24 16:22:38 +0100

    fix: update linear regression example to use StarModelTester

--- examples/flame/lin_reg_star.py
+++ examples/flame/lin_reg_star.py
@@ -1,7 +1,12 @@
-import numpy as np
-from flame.star import StarAggregator, StarAnalyzer, StarModel
+# FLAME implementation of the basic linear regression example in examples/standalone/minimal_lin_regression.py
 
-from fedstats import LinearRegression, MetaAnalysisAggregation
+import argparse
+
+import numpy as np
+from flame.star import StarAggregator, StarAnalyzer, StarModelTester
+
+from examples.standalone.minimal_lin_regression import make_local_data, run_regression
+from fedstats import MetaAnalysisAggregation
 
 
 class LocalLinearModel(StarAnalyzer):
@@ -25,22 +30,7 @@ class LocalLinearModel(StarAnalyzer):
                                    - Contains the result from the aggregator's aggregation_method in subsequent iterations.
         :return: Any result of your analysis on one node (ex. patient count).
         """
-        ## load data
-        # required to take first element as we have one data store and the list has therefore 1 entry
-        data = data[0]
-
-        feature_name = [name for name in data if name.startswith("X_node")][0]
-        target_name = [name for name in data if name.startswith("y_node")][0]
-
-        self.targets = np.fromstring(data[target_name], sep=" ", dtype=float)
-        self.features = np.fromstring(data[feature_name], sep=" ", dtype=float).reshape(
-            self.targets.size, -1
-        )
-
-        # run regression
-        reg = LinearRegression(self.features, self.targets)
-        reg.fit()
-        return reg.get_result()
+        return run_regression(data[0], data[1])
 
 
 class ResultsAggregator(StarAggregator):
@@ -61,9 +51,9 @@ class ResultsAggregator(StarAggregator):
         """
         agg = MetaAnalysisAggregation(analysis_results)
         agg.aggregate_results()
-        return agg.get_results()
+        return agg.get_aggregated_results()
 
-    def has_converged(self, result, last_result, num_iterations):
+    def has_converged(self, result, last_result):
         """
         Determines if the aggregation process has converged.
 
@@ -75,7 +65,7 @@ class ResultsAggregator(StarAggregator):
         return True  # Return True to indicate convergence in this simple analysis
 
 
-def main():
+def main(num_clients, num_obs_each, seed):
     """
     Sets up and initiates the distributed analysis using the FLAME components.
 
@@ -83,7 +73,13 @@ def main():
     - Specifies the type of data and queries to execute.
     - Configures analysis parameters like iteration behavior and output format.
     """
-    StarModel(
+    rng = np.random.default_rng(seed=seed)
+    seeds = rng.integers(0, 99999, size=num_clients)
+
+    datasets = [make_local_data(num_obs_each, seed) for seed in seeds]
+
+    StarModelTester(
+        data_splits=datasets,  # List of data splits for each analyzer node (not needed for this example as dummy data is generated within the analyzer)
         analyzer=LocalLinearModel,  # Custom analyzer class (must inherit from StarAnalyzer)
         aggregator=ResultsAggregator,  # Custom aggregator class (must inherit from StarAggregator)
         data_type="s3",  # Type of data source ('fhir' or 's3')
@@ -96,4 +92,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Number of clients and observations.")
+    parser.add_argument("--clients", type=int, default=5, help="Number of clients.")
+    parser.add_argument("--obs", type=int, default=100, help="Number of observations at each client.")
+    parser.add_argument("--seed", type=int, default=42, help="Seed to generate data.")
+    args = parser.parse_args()
+    main(args.clients, args.obs, args.seed)
